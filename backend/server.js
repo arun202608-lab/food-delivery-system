a36715a82100b7cfb4d0a8b5c22e7269026122ba
Arun <arun202608@gmail.com>
2026-09-06 11:50:05 +0530

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
-import 'dotenv/config'
+import "dotenv/config"
 import cartRouter from "./routes/cartRoute.js"
 import orderRouter from "./routes/orderRoute.js"
 
@@ -12,7 +12,13 @@ const app = express()
 const port = process.env.PORT || 4000
 
 app.use(express.json())
-app.use(cors())
+
+app.use(cors({
+    origin: "https://food-delivery-system-noig-nmog7mpzm-arun202608-labs-projects.vercel.app",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "token"]
+}))
 
 connectDB()
 
