c3eb1612662aedbbfbd31c8eb8bd635666c0de28
Arun <arun202608@gmail.com>
2026-09-06 13:24:04 +0530

    add admin panel

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import {assets} from "../../assets/assets"
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <a href="food-delivery-system-bice.vercel.app"><img src={assets.logo} alt="" className="logo" /></a>
+      <a href="#food-delivery-system-bice.vercel.app"><img src={assets.logo} alt="" className="logo" /></a>
       <img src={assets.profile_image} alt="" className="profile" />
     </div>
   )
